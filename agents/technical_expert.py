9d23287893ee89d4009b2dd8bb0dd2624f326f68
Nguyễn Dũng <nqdhocai@gmail.com>
2025-01-30 00:00:45 +0700

    Update technical_expert.py

--- agents/technical_expert.py
+++ agents/technical_expert.py
@@ -21,11 +21,11 @@ def technical_chat(question):
     blog_ids, chunk_contents = get_blog_by_query(question)
     context = "/n/n ".join(chunk_contents)
     prompt = f"""
-    Trả lời câu hỏi về kĩ thuật trong lĩnh vực mạng và thiết bị mạng dưới đây, sử dung thông tin được cung cấp dưới đây (nếu không có thông tin thì sử dụng kiến thức của bạn)
-    ### Question
-    {question}
-    ### Contexts
-    {context}
+    Answer the technical questions in the field of networking and network devices below, using the information provided below (if no information is provided, use your knowledge)
+### Question
+{question}
+### Contexts
+{context}
     """
     response = model.generate_content(prompt).text
     return {
